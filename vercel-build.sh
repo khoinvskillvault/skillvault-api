0ce85ca48343f183f862d6b9a74ba1cbb5b10ce2
khoinvskillvault <khoi.nguyenfb@gmail.com>
2026-04-23 21:41:22 +0700

    Update vercel-build.sh

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# 1. Tải trình cài đặt từ Vnstock
+# 1. Tải trình cài đặt
 wget -q https://vnstocks.com/files/vnstock-cli-installer.run -O installer.run
 chmod +x installer.run
 
-# 2. Chạy cài đặt không tương tác bằng API Key đã thiết lập ở Bước 1
-./installer.run -- --non-interactive --api-key "$VNSTOCK_API_KEY"
+# 2. ÉP cài đặt vào thư mục tên là 'vnstock_lib' ngay tại dự án
+./installer.run -- --non-interactive --api-key "$VNSTOCK_API_KEY" --venv-path "./vnstock_lib"
 
-# 3. Tiếp tục cài đặt các thư viện còn lại
+# 3. Cài các thứ còn lại
 pip install -r requirements.txt
